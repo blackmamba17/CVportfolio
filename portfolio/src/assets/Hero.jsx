7bbeb012afd5b266382c267c67fbc757fb4dd753
gtorrisi <scuolatorrisi@gmail.com>
2024-05-14 23:26:34 +0200

    2.9

--- portfolio/src/assets/Hero.jsx
+++ portfolio/src/assets/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 
 function Hero() {
@@ -11,11 +13,14 @@ function Hero() {
                 <Box sx={{ width: "60%", display: "flex", flexDirection: "column" }}>
                     <Box sx={{ p: 2 }}>
                         <h1>The only constant in the technology industry is change.</h1>
-                        <p>Hi! I'm Gabriele Torrisi, welcome to my website</p>
+                        <p>Hi! I'm Gabriele Torrisi, welcome to my website. Since in the very first years of my life, when other boys were playing with sticks and stones, I was already having a PC in my hand deploying retro server of an old popular chat game. Now, at 22 years old, I would like to tell you my path in IT not with words, but with my projects. So..</p>
+                        <Button variant="outlined" color="secondary" size='large'>test</Button>
                     </Box>
                 </Box>
                 <Box sx={{ width: "40%" }}>
-                    <Avatar alt="profile picture of my self" />
+                    <Avatar alt="profile picture of my self" src='/public/proPic.jpg' sx={{
+                        width: 300, height: 300, boxShadow: "0px 10px 13px -7px #000000"
+                    }} />
                 </Box>
             </Paper>
         </Box>
