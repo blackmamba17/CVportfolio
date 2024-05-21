1cc27e51ea72aba7cc04c4257f9c4ecf851820cf
gtorrisi <scuolatorrisi@gmail.com>
2024-05-21 13:59:44 +0200

    tetet

--- src/assets/Curriculum.jsx
+++ src/assets/Curriculum.jsx
@@ -17,19 +17,19 @@ function Curriculum() {
         date: "2020 - 2022",
         description: "Helping the client to carry out the internet network, vpn configuration and general troubleshooting.",
         website: "https://www.sielte.it/index.php/it/",
-        logo: "./CVportfolio/public/sielte.jpg"
+        logo: "public/sielte.jpg"
     }, {
         business: "MOVIA SPA",
         date: "2023 - current",
         description: "vpn, hardware & software troubleshooting and installation, mongoDB admin, creating full responsive html template for the client, testing new staging updates, checking new GitHub commit and more",
         website: "https://movia.biz/",
-        logo: "./CVportfolio/public/movia.jpeg"
+        logo: "public/movia.jpeg"
     }, {
         business: "FREELANCER",
         date: "2023 - current",
         description: "release live server with the application that the customer required",
         website: "http://localhost:5173/",
-        logo: "./CVportfolio/public/job2.PNG"
+        logo: "public/job2.PNG"
     }]
 
     return (
