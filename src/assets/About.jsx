4334a7c188528564b2bf6a21bc9ed1a2d4be1562
gtorrisi <scuolatorrisi@gmail.com>
2024-05-21 12:03:30 +0200

    public/

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -14,7 +14,7 @@ function About() {
                 I am a really enjoiable person, curious about everything and perfectly merged with the team work. I started my career in computer science even before the High School, using Hamachi to configure VPN and C++ to create basic shell programs. Growing up with the PS2, Nintendo DS, XBOX360 and an old crap Notebook, I started to develop hardware skills about repair and assembling pc. Later I found that I can create ad deploy MODs for old games to revive and to make them more funny. I was already having skills in server management and programming, so the question became naturally..       </p>
             <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "no-wrap", m: 2 }}>
                 <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", p: 2 }} >
-                    <img style={{ boxShadow: "10px 10px 13px -7px #000000", width: "100%" }} className='heroImage' src="/CVportfolio/public/heroGif2.gif" alt="programming image" />
+                    <img style={{ boxShadow: "10px 10px 13px -7px #000000", width: "100%" }} className='heroImage' src="public/heroGif2.gif" alt="programming image" />
                 </Box>
 
                 <Box style={{ textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
